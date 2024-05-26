797da189dd1e20928daf58483a30885b7f9cd461
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-05-26 23:35:54 +0530

    made some changes in main file and added routes

--- qtify/src/main.jsx
+++ qtify/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage.jsx';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
+      <Route path="" element={<HomePage />}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
