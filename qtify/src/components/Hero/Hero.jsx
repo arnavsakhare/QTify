632d67f0f99e5e0f202d01198821c0a44a047c03
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-05-26 23:36:51 +0530

    Hero Section

---
+++ qtify/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Headphones from "../.././assets/hero_headphones.png";
+import styles from "./Hero.module.css";
+
+function Hero() {
+  return (
+    <div className={styles.hero}>
+        <div >
+            <h1>100 Thousand Songs, ad free</h1>
+            <h1>Over thousand podcast episodes</h1>
+        </div>
+        <div>
+            <img src={Headphones} alt="HeadPhone Image" height="212px" width="212px"/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
